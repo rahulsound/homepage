28aba6407c64a4ad429f8b15f2dd3ebb7da511fd
Rahul Soundrarajan <rahulsound@gmail.com>
2024-08-06 01:48:36 +0530

    Typos

--- pages/Explora.py
+++ pages/Explora.py
@@ -36,8 +36,8 @@ def run_explora():
           ''')
     st.write(md)
 
-    st.markdown('''#### :red[Link to ongoing analysis with visualizations: [here](https://explora-ai-explainability.streamlit.app/)] ''')
-    st.markdown('''---''')
+    st.markdown('''#### :red[Link to ongoing analysis with visualizations: [here](https://explora-ai-explainability.streamlit.app/)] 
+                ---''')
 
 st.subheader(":blue[Rahul Soundrarajan]", anchor='#about')
 st.markdown('''

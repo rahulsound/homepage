284f6172e9fc77f0dc3a568149f3fca887ee2d8f
Rahul Soundrarajan <rahulsound@gmail.com>
2024-08-09 13:05:32 +0530

    Typos

--- pages/07Skills.py
+++ pages/07Skills.py
@@ -22,8 +22,8 @@ def run_skills_and_interests():
     st.header(":blue[Skills & Expertise]")
     st.markdown(''' 
 ###### :violet[**I am passionate about fostering innovation in the intersection of Telecom & AI/ML, Computer Vision & Sensor Fusion domains | [[filed patents]](https://patents.justia.com/inventor/rahul-soundrarajan)**]
-''')
-st.write('Reskilling is essential to keep-up with Tech and figure out how to do things better...') 
+''')    
+    st.write('Reskilling is essential to keep-up with Tech and figure out how to do things better...') 
 
 
     
